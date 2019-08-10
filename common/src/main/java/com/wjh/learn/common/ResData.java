6bae0a374a3079be2ab61309dec5cecaf1923c5b
JHW <1342633711@qq.com>
2019-08-10 08:04:08 +0800

    init project

---
+++ common/src/main/java/com/wjh/learn/common/ResData.java
@@ -0,0 +1,71 @@
+package com.wjh.learn.common;
+
+/**
+ * 返回值
+ */
+public class ResData {
+    private int code = 200;
+    private String msg;
+    private Object data;
+
+    public ResData(int code, String msg) {
+        this(code, msg, null);
+    }
+
+    public ResData(Object data) {
+        this.data = data;
+    }
+
+    public ResData(int code, String msg, Object data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public static ResData SUCCESS(Object data) {
+        ResData resData = new ResData(200, "success", data);
+        return resData;
+    }
+
+    public static ResData SUCCESS() {
+        return SUCCESS(null);
+    }
+
+    public static ResData fail(int code, String msg) {
+        ResData resData = new ResData(code, msg, null);
+        return resData;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "ResData{" +
+                "code=" + code +
+                ", msg='" + msg + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
