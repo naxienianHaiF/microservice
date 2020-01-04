e1ed4d377bd8cf9bfedfdaa1641c9edfac902835
JHW <1342633711@qq.com>
2020-01-04 17:35:26 +0800

    swagger ApiResponse annotation

--- user/src/main/java/com/wjh/learn/user/rest/api/TestApi.java
+++ user/src/main/java/com/wjh/learn/user/rest/api/TestApi.java
@@ -5,6 +5,7 @@ import com.wjh.learn.user.service.HystricTestService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,6 +24,7 @@ public class TestApi {
     @GetMapping("/test")
     @ApiOperation(value = "132", tags = "API test", notes = "测试ApiOperation")
     @ApiImplicitParam(name = "msg", value = "输入信息", dataType = "String")
+    @ApiResponse(code = 4002, message = "测试熔断", response = ResData.class)
     public ResData test(String msg) {
         return testService.say(msg);
     }
