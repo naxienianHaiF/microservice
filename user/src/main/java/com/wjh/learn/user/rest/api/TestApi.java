018ea5b48aad6d38c9ee4ddcdb61b45c14dbad3c
JHW <1342633711@qq.com>
2020-01-04 17:11:24 +0800

    add swagger function

--- user/src/main/java/com/wjh/learn/user/rest/api/TestApi.java
+++ user/src/main/java/com/wjh/learn/user/rest/api/TestApi.java
@@ -2,6 +2,9 @@ package com.wjh.learn.user.rest.api;
 
 import com.wjh.learn.common.ResData;
 import com.wjh.learn.user.service.HystricTestService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2019/9/25 21:54
  */
 @RestController
+@Api(tags = "test接口")
 public class TestApi {
 
     @Autowired
     private HystricTestService testService;
 
     @GetMapping("/test")
+    @ApiOperation(value = "132", tags = "API test", notes = "测试ApiOperation")
+    @ApiImplicitParam(name = "msg", value = "输入信息", dataType = "String")
     public ResData test(String msg) {
         return testService.say(msg);
     }
