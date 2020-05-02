aa97e697ff2d26864bbd356384114f34844b36a9
JHW <1342633711@qq.com>
2020-05-02 14:45:59 +0800

    import

--- servlet/src/main/java/com/wjh/learn/servlet/InnerErrorServlet.java
+++ servlet/src/main/java/com/wjh/learn/servlet/InnerErrorServlet.java
@@ -1,8 +1,6 @@
 package com.wjh.learn.servlet;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
