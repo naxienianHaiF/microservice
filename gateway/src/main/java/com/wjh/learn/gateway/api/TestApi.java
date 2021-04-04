b6a0644bc8d0503977c890e0febf63296ebadc84
WJH <1342633711@qq.com>
2021-04-04 20:34:13 +0800

    remove unused import

--- gateway/src/main/java/com/wjh/learn/gateway/api/TestApi.java
+++ gateway/src/main/java/com/wjh/learn/gateway/api/TestApi.java
@@ -2,7 +2,6 @@ package com.wjh.learn.gateway.api;
 
 import com.wjh.learn.common.ResData;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
